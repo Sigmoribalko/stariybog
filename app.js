d9af45a17568f889032a4dde651cd9432053f63f
Sigmoribalko <mkemke31269@gmail.com>
2024-06-18 13:35:39 +0300

    amigo

---
+++ app.js
@@ -0,0 +1,20 @@
+let pul = 1
+let firstname = "Stariy_Bog"
+const box = true
+
+firstname = 'iceberg'
+
+
+// can do
+//let $ , _ , 1234567890
+
+/* restricted
+let 34msa = 11
+let ds-ss = 1
+let const
+*/
+
+console.log(firstname);
+const submitBtn = document.getElementById("submit")
+
+console.log();
